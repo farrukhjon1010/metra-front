bd1323b5cc0a3b9882057c2d85514d224332a0dc
Muhammad <muhammadmavlonovkhujand@gmail.com>
2026-01-29 19:27:59 +0500

    feat: enhance history page with generation details and improve image handling

--- src/app/pages/history/history.component.ts
+++ src/app/pages/history/history.component.ts
@@ -1,29 +1,97 @@
-import {Component, OnInit} from '@angular/core';
-import {ApiService} from '../../core/services/api.service';
-import {NgStyle} from '@angular/common';
+import {ChangeDetectorRef, Component, Input, OnChanges, SimpleChanges, OnInit} from '@angular/core';
+import {DatePipe, NgStyle} from '@angular/common';
 import {ButtonComponent} from '../../shared/components/button/button.component';
 import {Router} from '@angular/router';
+import {CreateCard} from '../create/create.component';
+import {GenerationService} from '../../core/services/generation.service';
+import {GenerationType} from '../../core/models/generation.model';
 
 @Component({
   selector: 'app-history',
-  imports: [NgStyle, ButtonComponent],
+  standalone: true,
+  imports: [NgStyle, ButtonComponent, DatePipe],
   templateUrl: './history.component.html',
   styleUrls: ['./history.component.scss'],
 })
-export class HistoryComponent implements OnInit {
+export class HistoryComponent implements OnChanges, OnInit {
 
+  @Input() card!: CreateCard;
+  UUID: string = '23edfdb2-8ab1-4f09-9f3b-661e646e3965';
   selectedFilter: 'all' | 'photo' | 'video' = 'all';
-  history: any[] = [];
+  generationHistory: any[] = [];
 
-  constructor(private api: ApiService, private router: Router) {}
+  constructor(
+    private router: Router,
+    private generationService: GenerationService,
+    private cdr: ChangeDetectorRef) {
+  }
 
   ngOnInit() {
-    this.api.getScenes().subscribe((data: any) => {
-      this.history = data;
+    this.loadGenerationsHistory();
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['card'] && this.card) {
+      this.loadGenerationsHistory();
+    }
+  }
+
+  loadGenerationsHistory() {
+    const type = this.card?.type;
+    this.generationService.findByUser(this.UUID, type).subscribe({
+      next: (data) => {
+        this.generationHistory = data;
+        this.cdr.markForCheck();
+        console.log('Генерации загружены:', data);
+      },
+      error: (err) => {
+        console.error('Ошибка при загрузке:', err);
+      }
     });
   }
 
   navigateToCreate() {
     this.router.navigate(['/history/improving-quality']);
   }
+
+  downloadFile(url: string, type: 'image' | 'video') {
+    fetch(url)
+      .then(res => res.blob())
+      .then(blob => {
+        const a = document.createElement('a');
+        const objectUrl = URL.createObjectURL(blob);
+
+        a.href = objectUrl;
+        a.download = type === 'image'
+          ? 'generation-image.jpg'
+          : 'generation-video.mp4';
+
+        a.click();
+        URL.revokeObjectURL(objectUrl);
+      })
+      .catch(err => {
+        console.error('Ошибка загрузки', err);
+      });
+  }
+
+  goToUpscale(imageUrl: string) {
+    this.router.navigate(
+      ['/history/improving-quality'],
+      {state: {imageUrl}}
+    );
+  }
+
+  repeatGeneration(generation: any) {
+    const generationType: GenerationType = generation.type;
+
+    this.router.navigate(['/create'], {
+      state: {
+        id: generation.id,
+        prompt: generation.prompt,
+        imageUrl: generation.imageURL,
+        type: generationType
+      }
+    });
+  }
+
 }
--- src/app/pages/history/improving-quality/improving-quality.ts
+++ src/app/pages/history/improving-quality/improving-quality.ts
@@ -1,6 +1,8 @@
-import {ChangeDetectorRef, Component, ElementRef, ViewChild} from '@angular/core';
+import {AfterViewInit, ChangeDetectorRef, Component, ElementRef, ViewChild} from '@angular/core';
 import {Router} from '@angular/router';
 import {ButtonComponent} from '../../../shared/components/button/button.component';
+import {UpscaleService} from '../../../core/services/upscale.service';
+import {firstValueFrom} from 'rxjs';
 
 @Component({
   selector: 'app-improving-quality',
@@ -8,67 +10,103 @@ import {ButtonComponent} from '../../../shared/components/button/button.componen
   templateUrl: './improving-quality.html',
   styleUrls: ['./improving-quality.scss'],
 })
-export class ImprovingQuality {
+export class ImprovingQuality implements AfterViewInit {
 
-  @ViewChild('photoInput', { static: false }) photoInput!: ElementRef<HTMLInputElement>;
+  @ViewChild('photoInput', {static: false}) photoInput!: ElementRef<HTMLInputElement>;
 
-  photos: { photo: string | null } = { photo: null };
-  enhancedPhoto: string | null = null;
   isProcessing = false;
   isStarted = false;
+  photos: { photo: string | null } = {photo: null};
+  originalImage: string | null = null;
+  improvedImage: string | null = null;
 
   constructor(private router: Router,
-              private cdr: ChangeDetectorRef,) {
+              private cdr: ChangeDetectorRef,
+              private upscaleService: UpscaleService) {
+  }
+
+  ngAfterViewInit() {
+    const imageUrl = history.state?.imageUrl;
+    if (imageUrl) {
+      this.photos.photo = imageUrl;
+      this.cdr.detectChanges();
+    }
   }
 
   goBack() {
     this.router.navigate(['/history']);
   }
 
-  onPhotoSelected(event: Event, type: 'photo' ) {
-    const photo = (event.target as HTMLInputElement)?.files?.[0];
-    if (!photo) return;
+  onPhotoSelected(event: Event, type: 'photo') {
+    const file = (event.target as HTMLInputElement)?.files?.[0];
+    if (!file) return;
+    this.cdr.detectChanges();
 
     const reader = new FileReader();
     reader.onload = () => {
       this.photos[type] = reader.result as string;
+
+      this.originalImage = null;
+      this.improvedImage = null;
+      this.isStarted = false;
+
       this.cdr.detectChanges();
     };
-    reader.readAsDataURL(photo);
-    this.cdr.detectChanges();
+    reader.readAsDataURL(file);
   }
 
   triggerFileInput(type: 'photo', event: Event) {
     event.stopPropagation();
-    const map = {
-      photo: this.photoInput,
-    };
+    const map = {photo: this.photoInput};
     map[type]?.nativeElement.click();
   }
 
   removePhoto(type: 'photo', event: Event) {
     event.stopPropagation();
     this.photos[type] = null;
+    this.originalImage = null;
+    this.improvedImage = null;
+    this.isStarted = false;
   }
 
-  enhanceImage() {
+  async enhanceImage() {
     if (!this.photos.photo) return;
 
     this.isStarted = true;
+    this.originalImage = this.photos.photo;
+    this.improvedImage = null;
     this.isProcessing = true;
+    this.cdr.detectChanges();
 
-    this.enhancedPhoto = this.photos.photo;
-    setTimeout(() => {
-      this.enhancedPhoto = this.photos.photo;
+    try {
+      const result = await firstValueFrom(this.upscaleService.improveImage(this.originalImage));
+      this.improvedImage = result?.improvedImage || this.originalImage;
+    } catch (err) {
+      console.error('Ошибка улучшения изображения', err);
+      this.improvedImage = null;
+    } finally {
       this.isProcessing = false;
-    }, 1200);
+      this.cdr.detectChanges();
+    }
   }
 
   downloadImage() {
+    if (!this.improvedImage) return;
 
+    fetch(this.improvedImage)
+      .then(res => res.blob())
+      .then(blob => {
+        const a = document.createElement('a');
+        a.href = URL.createObjectURL(blob);
+        a.download = 'improved-image.jpg';
+        a.click();
+        URL.revokeObjectURL(a.href);
+      })
+      .catch(err => console.error('Ошибка при скачивании улучшенного изображения', err));
   }
 
   repeatImage() {
-
+    this.improvedImage = null;
+    this.isStarted = false;
   }
 }
