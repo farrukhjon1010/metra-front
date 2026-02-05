730859bcaf59143399007202fab297b61f97ff0a
Muhammad <muhammadmavlonovkhujand@gmail.com>
2026-02-05 16:41:59 +0500

    feat: add profile main component with avatar display, balance, subscription, and affiliate program sections

---
+++ src/app/pages/profile/profile-main/profile-main-component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ProfileMainComponent } from './profile-main-component';
+
+describe('ProfileMain', () => {
+  let component: ProfileMainComponent;
+  let fixture: ComponentFixture<ProfileMainComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ProfileMainComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ProfileMainComponent);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/pages/profile/profile-main/profile-main-component.ts
@@ -0,0 +1,77 @@
+import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
+import {ButtonComponent} from '../../../shared/components/button/button.component';
+import {Router} from '@angular/router';
+import {AvatarService} from '../../../core/services/avatar.service';
+import {ReferralService} from '../../../core/services/balance.service';
+
+@Component({
+  selector: 'app-profile-main',
+  imports: [
+    ButtonComponent
+  ],
+  templateUrl: './profile-main-component.html',
+  styleUrls: ['./profile-main-component.scss'],
+})
+export class ProfileMainComponent implements OnInit {
+
+  UUID: string = '23edfdb2-8ab1-4f09-9f3b-661e646e3965';
+
+  selectedAvatars: string[] = [];
+  isLoading: boolean = false;
+  income = 0;
+  currency = '';
+
+  constructor(
+    public router: Router,
+    private avatarService: AvatarService,
+    private referralService: ReferralService,
+    private cdr: ChangeDetectorRef
+  ) {}
+
+  ngOnInit() {
+    this.loadUserAvatars();
+
+    this.referralService.income$.subscribe(data => {
+      this.income = data.income;
+      this.currency = data.currency;
+      this.cdr.detectChanges();
+    });
+  }
+
+  loadUserAvatars() {
+    const userId = this.UUID;
+    this.isLoading = true;
+
+    this.avatarService.findByUser(userId).subscribe({
+      next: (avatar) => {
+        if (avatar && avatar.imagesURL) {
+          this.selectedAvatars = [...avatar.imagesURL];
+        } else {
+          this.selectedAvatars = [];
+        }
+        this.isLoading = false;
+        this.cdr.detectChanges();
+      },
+      error: () => {
+        this.isLoading = false;
+        this.cdr.detectChanges();
+      }
+    });
+  }
+
+  goToAddAvatar() {
+    this.router.navigate(['/profile/add-avatar']);
+  }
+
+  goToReplenish() {
+    this.router.navigate(['/profile/balance']);
+  }
+
+  goToAffiliateProgram() {
+    this.router.navigate(['/profile/affiliate-program']);
+  }
+
+  goToSubscription() {
+    this.router.navigate(['/profile/subscription']);
+  }
+}
