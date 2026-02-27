fe842a320e96f51242faaf816770d78f810c8e96
Muhammad <muhammadmavlonovkhujand@gmail.com>
2026-02-27 18:32:03 +0500

    feat: add loading overlay with signal-based state in SplashCaseComponent; improve avatar loading and UI feedback

--- src/app/pages/splash/components/splash-case/splash-case.component.ts
+++ src/app/pages/splash/components/splash-case/splash-case.component.ts
@@ -1,14 +1,16 @@
-import {Component, EventEmitter, Output, inject, computed, ChangeDetectionStrategy} from '@angular/core';
+import { Component, EventEmitter, Output, inject, computed, ChangeDetectionStrategy, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonComponent } from '../../../../shared/components/button/button.component';
 import { AvatarService } from '../../../../core/services/avatar.service';
-import { catchError, of } from 'rxjs';
+import { catchError, of, tap } from 'rxjs';
 import { toSignal } from '@angular/core/rxjs-interop';
+import { Loading } from '../../../../shared/components/loading/loading';
+import { ChangeDetectorRef } from '@angular/core';
 
 @Component({
   selector: 'app-splash-case',
   standalone: true,
-  imports: [CommonModule, ButtonComponent],
+  imports: [CommonModule, ButtonComponent, Loading],
   templateUrl: './splash-case.component.html',
   styleUrls: ['./splash-case.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
@@ -19,11 +21,20 @@ export class SplashCaseComponent {
   @Output() demo = new EventEmitter<void>();
 
   private avatarService = inject(AvatarService);
+  private cdr = inject(ChangeDetectorRef);
+
+  readonly isLoading = signal(true);
 
   private avatarsSignal = toSignal(
     this.avatarService.findByUser().pipe(
+      tap(() => {
+        this.isLoading.set(false);
+        this.cdr.markForCheck();
+      }),
       catchError(err => {
         console.error('Ошибка загрузки аватаров', err);
+        this.isLoading.set(false);
+        this.cdr.markForCheck();
         return of({ imagesURL: [] });
       })
     ),
